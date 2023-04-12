4115cd0c9d97a3677deb2467496a8cee97c68dec
Anilrocky <anilkumarac96@gmail.com>
2023-04-12 13:53:44 +0530

    [Anil] Add:Implement CSV handling

---
+++ CSVHelperDemo/Operation.cs
@@ -0,0 +1,38 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVHelperDemo
+{
+    public class Operation
+    {
+        public static void ImplementCSVHandling()
+        {
+            List<Model> records = new List<Model>();
+            string importFilePath = @"D:\BridgeLabz\CSVHelperDemo\CSVHelperDemo\data.csv";
+            string exportFilePath = @"D:\BridgeLabz\CSVHelperDemo\CSVHelperDemo\csvFile.csv";
+            using (var reader = new StreamReader(importFilePath))
+            using (var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                records = csvReader.GetRecords<Model>().ToList();
+                Console.WriteLine("Reading CSV File");
+                Console.WriteLine("=========================");
+                foreach (var data in records)
+                {
+                    Console.WriteLine(data.name + "\n" + data.email + "\n" + data.phone + "\n" + data.country);
+                }
+            }
+            using (var writer = new StreamWriter(exportFilePath))
+            using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csvWriter.WriteRecords(records);
+            }
+        }
+    }
+}
--- CSVHelperDemo/Program.cs
+++ CSVHelperDemo/Program.cs
@@ -4,8 +4,22 @@ namespace CSVHelperDemo
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome");
+        {           
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Implement CSV Handling \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Operation.ImplementCSVHandling();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            } 
         }
     }
 }
